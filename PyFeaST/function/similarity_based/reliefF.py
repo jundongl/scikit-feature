893f33e0dd4f293e2d5f5fde0f8a45d97d66646c
jundongl <jundongl@asu.edu>
2015-11-06 10:18:56 -0700

    update

--- PyFeaST/function/similarity_based/reliefF.py
+++ PyFeaST/function/similarity_based/reliefF.py
@@ -1,12 +1,11 @@
 import numpy as np
-from PyFeaST.utility.construct_W import construct_W
+from random import randrange
+from sklearn.metrics.pairwise import pairwise_distances
 
 
-def reliefF(X, y):
+def reliefF(X, y, **kwargs):
     """
-    This function implements the reliefF feature selection, steps are as follows:
-    1. Construct the affinity matrix W in reliefF way
-    2. For the r-th feature, we define fr = X(:,r), reliefF score for the r-th feature is -1+fr'*W*fr
+    This function implements the reliefF feature selection
 
     Input
     -----
@@ -14,7 +13,10 @@ def reliefF(X, y):
         input data
     y: {numpy array}, shape (n_samples,)
         input class labels
-
+    kwargs: {dictionary}
+        parameters of reliefF:
+        k: {int}
+            choices for the number of neighbors (default k = 5)
     Output
     ------
     score: {numpy array}, shape (n_features,)
@@ -25,13 +27,79 @@ def reliefF(X, y):
     Zhao, Zheng et al. "On Similarity Preserving Feature Selection." TKDE 2013.
     """
 
-    # construct the affinity matrix W
-    kwargs = {"neighbor_mode": "supervised", "reliefF": True, 'y': y}
-    W = construct_W(X, **kwargs)
+    if "k" not in kwargs.keys():
+        k = 5
+    else:
+        k = kwargs["k"]
     n_samples, n_features = X.shape
+
+    # calculate pairwise distances between instances
+    distance = pairwise_distances(X)
+
     score = np.zeros(n_features)
-    for i in range(n_features):
-        score[i] = -1 + np.dot(np.transpose(X[:, i]), W.dot(X[:, i]))
+
+    # the number of sampled instances is equal to the number of total instances
+    for iter in range(n_samples):
+        idx = randrange(0, n_samples, 1)
+        near_hit = []
+        near_miss = dict()
+
+        self_fea = X[idx, :]
+        c = np.unique(y).tolist()
+
+        stop_dict = dict()
+        for label in c:
+            stop_dict[label] = 0
+        del c[c.index(y[idx])]
+
+        p_dict = dict()
+        p_label_idx = float(len(y[y == y[idx]]))/float(n_samples)
+
+        for label in c:
+            p_label_c = float(len(y[y == label]))/float(n_samples)
+            p_dict[label] = p_label_c/(1-p_label_idx)
+            near_miss[label] = []
+
+        distance_sort = []
+        distance[idx, idx] = np.max(distance[idx, :])
+
+        for i in range(n_samples):
+            distance_sort.append([distance[idx, i], int(i), y[i]])
+        distance_sort.sort(key=lambda x: x[0])
+
+        for i in range(n_samples):
+            # find k nearest hit points
+            if distance_sort[i][2] == y[idx]:
+                if len(near_hit) < k:
+                    near_hit.append(distance_sort[i][1])
+                elif len(near_hit) == k:
+                    stop_dict[y[idx]] = 1
+            else:
+                # find k nearest miss points for each label
+                if len(near_miss[distance_sort[i][2]]) < k:
+                    near_miss[distance_sort[i][2]].append(distance_sort[i][1])
+                else:
+                    if len(near_miss[distance_sort[i][2]]) == k:
+                        stop_dict[distance_sort[i][2]] = 1
+            stop = True
+            for (key, value) in stop_dict.items():
+                    if value != 1:
+                        stop = False
+            if stop:
+                break
+
+        # update reliefF score
+        near_hit_term = np.zeros(n_features)
+        for ele in near_hit:
+            near_hit_term = np.array(abs(self_fea-X[ele, :]))+np.array(near_hit_term)
+
+        near_miss_term = dict()
+        for (label, list) in near_miss.items():
+            near_miss_term[label] = np.zeros(n_features)
+            for ele in list:
+                near_miss_term[label] = np.array(abs(self_fea-X[ele, :]))+np.array(near_miss_term[label])
+            score += near_miss_term[label]/(k*p_dict[label])
+        score -= near_hit_term/k
     return score
 
 
@@ -42,4 +110,9 @@ def feature_ranking(score):
     """
     idx = np.argsort(score, 0)
     return idx[::-1]
+
+
+
+
+
 
