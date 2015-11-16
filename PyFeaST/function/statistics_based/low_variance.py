afa3ce45a7c359e79a382445da5d1013df9933f5
jundongl <jundongl@asu.edu>
2015-11-16 14:22:57 -0700

    update

--- PyFeaST/function/statistics_based/low_variance.py
+++ PyFeaST/function/statistics_based/low_variance.py
@@ -1,7 +1,7 @@
 from sklearn.feature_selection import VarianceThreshold
 
 
-def low_variance_feature_selection(X, p):
+def low_variance_feature_selection(X, threshold):
     """
     This function implements the low_variance feature selection (existing method in scikit-learn)
 
@@ -17,5 +17,5 @@ def low_variance_feature_selection(X, p):
     X_new: {numpy array}, shape (n_samples, n_selected_features)
         data with selected features
     """
-    sel = VarianceThreshold(threshold=(p * (1 - p)))
+    sel = VarianceThreshold(threshold)
     return sel.fit_transform(X)
