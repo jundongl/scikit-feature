1ebf31e67c11010a48c9fc220f38ad72f4f6cf85
Chapman Siu <chpmn.siu@gmail.com>
2017-06-13 10:35:56 +1000

    changed chisq and tests

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,7 +5,12 @@ The format is based on [Keep a Changelog](http://keepachangelog.com/)
 and this project adheres to [Semantic Versioning](http://semver.org/).
 
 ## [v0.0.1] - 2017-06-XX
+
 ### Added
 
 *  Python 3 support
+*  integration with nosetests
 
+### Changed
+
+*  Broke old API in favour of maintaining `scikit-learn` 0.18 compatibility and `SelectKBest`
--- skfeature/tests/chi_square_tests.py
+++ skfeature/tests/chi_square_tests.py
@@ -1,49 +1,35 @@
+from nose.tools import *
 import scipy.io
-from sklearn.metrics import accuracy_score
-from sklearn import cross_validation
 from sklearn import svm
 from skfeature.function.statistical_based import chi_square
+from sklearn.model_selection import KFold
+from sklearn.model_selection import cross_val_score
+from sklearn.feature_selection import SelectKBest
+from sklearn.pipeline import Pipeline
 
-
-def main():
+def test_chi2():
     # load data
-    mat = scipy.io.loadmat('../data/BASEHOCK.mat')
+    import os
+    print(os.getcwd())
+    mat = scipy.io.loadmat('./data/BASEHOCK.mat')
     X = mat['X']    # data
     X = X.astype(float)
     y = mat['Y']    # label
     y = y[:, 0]
     n_samples, n_features = X.shape    # number of samples and number of features
-
-    # split data into 10 folds
-    ss = cross_validation.KFold(n_samples, n_folds=10, shuffle=True)
-
+    
     # perform evaluation on classification task
     num_fea = 100    # number of selected features
-    clf = svm.LinearSVC()    # linear SVM
+    
+    # build pipeline
+    pipeline = []
+    pipeline.append(('select top k', SelectKBest(score_func=chi_square.chi_square, k=num_fea)))
+    pipeline.append(('linear svm', svm.LinearSVC()))
+    model = Pipeline(pipeline)
+    
+    # split data into 10 folds
+    kfold = KFold(n_splits=10, shuffle=True)
+    
+    results = cross_val_score(model, X, y, cv=kfold)
+    assert_true(results.mean() > 0.95)
 
-    correct = 0
-    for train, test in ss:
-        # obtain the chi-square score of each feature
-        score = chi_square.chi_square(X, y)
-
-        # rank features in descending order according to score
-        idx = chi_square.feature_ranking(score)
-
-        # obtain the dataset on the selected features
-        selected_features = X[:, idx[0:num_fea]]
-
-        # train a classification model with the selected features on the training dataset
-        clf.fit(selected_features[train], y[train])
-
-        # predict the class labels of test data
-        y_predict = clf.predict(selected_features[test])
-
-        # obtain the classification accuracy on the test data
-        acc = accuracy_score(y[test], y_predict)
-        correct = correct + acc
-
-    # output the average classification accuracy over all 10 folds
-    print(('Accuracy:', float(correct)/10))
-
-if __name__ == '__main__':
-    main()
